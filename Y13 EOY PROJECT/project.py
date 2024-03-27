1aef5d5f58899730403578cd6855d55dbba15af1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-28 08:46:26 +1300

    pizza

--- Y13 EOY PROJECT/project.py
+++ Y13 EOY PROJECT/project.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    # Render the home page
-    return render_template("home.html", pagename="home")
+    conn = sqlite3.connect('pizza.db')
+    cur= conn.cursor()
+    cur.execute("SELECT * FROM Pizza")
+    Pizzas = cur.fetchall()
+    return render_template("all-pizzas.html",pizzas = Pizzas)
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route('/pizza/<int:id>')
+def pizza(id):
+    conn = sqlite3.connect('pizza.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Pizza WHERE id = ?",(id,))
+    pizza = cur.fetchone()
+    return render_template("pizza.html", pizza = pizza)
 
-
-if __name__ == "__main__":  # type:ignore
+if __name__ == "__main__":
     app.run(debug=True)
